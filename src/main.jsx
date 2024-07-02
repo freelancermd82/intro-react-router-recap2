708b2b898fa831f5e358b35c86717b79bb29bd8a
Your Name <freelancermd82@gmail.com>
2024-07-02 16:04:04 +0600

    active loading not found

--- src/main.jsx
+++ src/main.jsx
@@ -57,6 +57,10 @@ const router = createBrowserRouter([
       {
         path: 'login',
         element: <Login></Login>
+      },
+      {
+        path: '*',
+        element: <div>404</div>
       }
     ]
   }
